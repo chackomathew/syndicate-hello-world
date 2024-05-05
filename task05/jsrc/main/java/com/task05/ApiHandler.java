51f742adb8500225a0e9eb060afd46c9393abed0
Chacko Mathew <chacko_mathew@epam.com>
2024-05-06 00:50:57 +0530

    tast05

---
+++ task05/jsrc/main/java/com/task05/ApiHandler.java
@@ -0,0 +1,70 @@
+package com.task05;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.PutItemResult;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.model.RetentionSetting;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@LambdaHandler(lambdaName = "api_handler",
+	roleName = "api_handler-role",
+	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+public class ApiHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    private final AmazonDynamoDB dynamoDb = AmazonDynamoDBClientBuilder.defaultClient();
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+        Map<String, AttributeValue> item = new HashMap<>();
+        String eventId = UUID.randomUUID().toString();
+        item.put("id", new AttributeValue().withS(eventId));
+
+        Map<String, String> body = null;
+        Map<String, String> content = null;
+        try {
+            body = objectMapper.readValue(input.getBody(), Map.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        String principalId = body.get("principalId");
+        try {
+            content = objectMapper.readValue(body.get("content"), Map.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        item.put("principalId", new AttributeValue().withN(principalId));
+        item.put("createdAt", new AttributeValue().withS(Instant.now().toString()));
+        try {
+            item.put("body", new AttributeValue().withS(objectMapper.writeValueAsString(content)));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        PutItemResult result = dynamoDb.putItem("Events", item);
+
+        Map<String, String> response = new HashMap<>();
+        try {
+            response.put("event", objectMapper.writeValueAsString(result.getAttributes()));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new APIGatewayProxyResponseEvent()
+                .withStatusCode(201)
+                .withBody(response.toString());
+	}
+}
